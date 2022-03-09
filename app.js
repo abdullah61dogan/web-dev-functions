e8cc45c41a456aab82aa5bef87523e28ad64e27d
Abdullah Doğan <abdullah.dogan@codeck.com.tr>
2022-03-09 18:15:26 +0300

    functions tekrar

---
+++ app.js
@@ -0,0 +1,17 @@
+
+//let topla = (say1,say2) => say1 + say2;
+//console.log(topla(10,2));
+/*
+let person1 = {name:'apo', age: 24};
+let person2 = {name:'doğan', age: 23};
+let sayHi = function(){
+    let person3 = {name:'doğan', age: 23};
+    console.log('Hi, '+ this.name);
+};
+sayHi.call(person1);
+sayHi();
+*/
+let myFunc = function(name){
+    console.log(name);
+}
+myFunc('abdullah');
